1addfe92a002381a6ce2e8ced324fb7725f047b3
complex <107022425+cccomplex@users.noreply.github.com>
2023-01-14 17:04:06 -0600

    update: "utility/.lua" `Utility:Create()` function

--- utility/.lua
+++ utility/.lua
@@ -1,3 +1,23 @@
 local Utility = {}
 
+function Utility:Create(Name, Parent, Properties)
+    if typeof(Parent) == "table" then
+        Parent, Properties = Properties, Parent
+    end
+
+    local Object; do
+        if Parent ~= nil then
+            Object = Instance.new(Name, Parent)
+        else
+            Object = Instance.new(Name, workspace)
+        end
+    end
+
+    for Property, Value in next, Properties do
+        Object[Property] = Value
+    end
+
+    return Object
+end
+
 return Utility
